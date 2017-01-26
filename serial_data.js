d68af99f5de1f6b3a56336946b607c9e0cee553c
doe1 <doedoe3321@gmail.com>
2017-01-26 15:00:08 +0900

    コード修正版

--- serial_data.js
+++ serial_data.js
@@ -1,6 +1,6 @@
 var serialPort = require('serialPort').SerialPort;
 
-var sp =new serialPort.SerialPort("/dev/ttyUSB1",{
+var sp =new serialPort.SerialPort("/dev/xxxx",{ //xxxx:自分の刺しているポート名
     baudrate: 115200,
     parser:serialPort.parsers.readline("\n")
 });
